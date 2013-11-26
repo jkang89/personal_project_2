fb892c506da3cb16b72404401c07ea8f2c3671cc
Jee Kang <jkang@TheBoss.local>
2013-11-25 17:22:39 -0800

    added filters and began working on frontend

--- flask_template/my_app.py
+++ flask_template/my_app.py
@@ -9,6 +9,10 @@ app.secret_key = "shhhthisisasecret"
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
--- flask_template/static/js/webcam3.js
+++ flask_template/static/js/webcam3.js
@@ -1,3 +1,5 @@
+var current_filter = null;
+
 var camera = (function(){
     var video, canvas, context, videoPause, overlayContext;
     var render_timer = null;
@@ -27,6 +29,7 @@ var camera = (function(){
         canvas.setAttribute('width', 640);
         canvas.setAttribute('height', 480);
         context = canvas.getContext("2d");
+
 
         //canvas.appendChild(video)
         //video.appendChild(canvas);
@@ -69,11 +72,19 @@ var camera = (function(){
     };
 
     function copy_frame() {
-        var video = $("video")[0];
-        var canvas = $("canvas")[0];
+        var video = $("#video")[0];
+        var mycanvas = $("#mycanvas")[0];
+        var bgcanvas = $("#bgcanvas")[0];
+        var my_ctx = mycanvas.getContext("2d");
+        var bg_ctx = bgcanvas.getContext("2d");
 
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(video, 0, 0);
+        bg_ctx.drawImage(video, 0, 0);
+
+        var pixels = bg_ctx.getImageData(0,0, video.width, video.height);
+
+        filterCanvas(pixels);
+        my_ctx.putImageData(pixels, 0, 0);
+        //my_ctx.drawImage(pixels, 0, 0);
 
     };
 
@@ -88,47 +99,36 @@ var camera = (function(){
     
 })();
 
-Filters = {};
-Filters.getPixels = function(video) {
-    var video = $('video')[0];
-    var canvas = $("canvas")[0];
-    var ctx = video.getContext('2d');
-    return ctx.getImageData(0,0,640,480);
-};
 
-Filters.filterImage = function(filter, image, var_args) {
-    var args = [this.getPixels(video)];
-    for (var i=2; i<arguments.length; i++) {
-        args.push(arguments[i]);
+filterCanvas = function(pixels) {
+    if (current_filter != null) {
+        current_filter(pixels);
     }
-    return filter.apply(null, args);
 };
 
-Filters.grayscale = function(pixels, args) {
-    //not working because .data can't read data off of something that is not defined
-    //need to define pixels...
+grayscale = function(pixels, args) {
+    //pixels is not defined
     var d = pixels.data;
-    for (var i=0; i<d.length; i+=4) {
+    for (var i = 0; i<d.length; i += 4) {
         var r = d[i];
         var g = d[i+1];
         var b = d[i+2];
-        var v = 0.2126*r + 0.7152*g + 0.0722*b;
-        d[i] = d[i+1] = d[i+2] = v 
+        d[i] = d[i+1] = d[i+2] = (r+g+b)/3;
     }
     return pixels;
 };
 
-Filters.brightness = function(pixels, adjustment) {
+brightness = function(pixels, args) {
     var d = pixels.data;
     for (var i=0; i<d.length; i+=4) {
-        d[i] += adjustment;
-        d[i+1] += adjustment;
-        d[i+2] += adjustment;
+        d[i] += 1;
+        d[i+1] += 1;
+        d[i+2] += 1;
     }
     return pixels;
 };
 
-Filters.sepia = function(pixels, args) {
+sepia = function(pixels, args) {
     var d = pixels.data;
     for (var i = 0; i < d.length; i += 4) {
         var r = d[i];
@@ -139,6 +139,58 @@ Filters.sepia = function(pixels, args) {
         d[i+2] = (r*0.272) + (g*0.534) + (b*0.131);
     }
     return pixels;
+};
+
+red = function(pixels, args) {
+    var d = pixels.data;
+    for (var i = 0; i < d.length; i += 4) {
+        var r = d[i];
+        var g = d[i+1];
+        var b = d[i+2];
+        d[i] = Math.min(255, d[i] * 2);
+        d[i+1] = d[i+1]/2;
+        d[i+2] = d[i+2]/2;
+    }
+    return pixels;
+};
+
+blue = function(pixels, args) {
+    var d = pixels.data;
+    for (var i = 0; i < d.length; i += 4) {
+        var r = d[i];
+        var g = d[i+1];
+        var b = d[i+2];
+        d[i] = d[i]/2;
+        d[i+1] = d[i+1]/2;
+        d[i+2] = Math.min(255, d[i+2] * 2)
+    }
+    return pixels
+};
+
+green = function(pixels, args) {
+    var d = pixels.data;
+    for (var i = 0; i < d.length; i += 4) {
+        var r = d[i];
+        var g = d[i+1];
+        var b = d[i+2];
+        d[i] = d[i]/2;
+        d[i+1] = Math.min(255, d[i+1] * 2);
+        d[i+2] = d[i+2]/2;
+    }
+    return pixels
+};
+
+threshold = function(pixels, args) {
+    //doesn't work
+    var d = pixels.data;
+    for (var i = 0; i < d.length; i += 4) {
+        var r = d[i];
+        var g = d[i+1];
+        var b = d[i+2];
+        var v = (0.2126*r + 0.7152*g + 0.0722*b >= threshold) ? 255 : 0;
+        d[i] = d[i+1] = d[i+2] = v
+    }
+    return pixels
 };
 
 function startup_stuff() {
@@ -159,9 +211,33 @@ $("#copy-to-canvas").click(function() {
 });
 
 $("#grayscale").click(function() {
-    Filters.grayscale();
+    current_filter = grayscale;
 });
 
 $("#brighten").click(function() {
-    Filters.brightness();
+    current_filter = brightness;
+});
+
+$("#sepia").click(function() {
+    current_filter = sepia;
+});
+
+$("#red").click(function() {
+    current_filter = red;
+});
+
+$("#blue").click(function() {
+    current_filter = blue;
+});
+
+$("#green").click(function() {
+    current_filter = green;
+});
+
+$("#normal").click(function() {
+    current_filter = null;
+});
+
+$("#threshold").click(function() {
+    current_filter = threshold;
 });
