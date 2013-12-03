74fd0bb31207fb4830245c60ce62155b76351177
Jee Kang <jkang@TheBoss.local>
2013-12-02 17:33:41 -0800

    added name, navbar, prettied it up

--- flask_template/my_app.py
+++ flask_template/my_app.py
@@ -37,5 +37,9 @@ def camera():
 def register():
     return render_template("register.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
